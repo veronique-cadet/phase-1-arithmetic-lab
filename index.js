a970311d800788507e8f0fc56fceb701137b0610
Veronique Cadet <cadetvero@gmail.com>
2022-11-21 06:47:46 -0500

    commit tree e36dd5ecce6ddb4bf6d31ab08103d5647455136f

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+const num3 = 16;
+const num4 = 4;
+const mod = num3 / num4;
+console.log(mod);
+
+function getRandom() {
+  return Math.random();
+}
